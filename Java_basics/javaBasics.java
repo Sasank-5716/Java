13859d2c7a97f1d12ce1c3a9216f6c2a7ff56663
Sasank5716 <sasanklama5716@gmail.com>
2025-06-16 16:54:28 +0545

    Add java basic theory (variables, datatypes, operators)

---
+++ Java_basics/javaBasics.java
@@ -0,0 +1,24 @@
+/*
+1. Structure of a Java program:
+-Every Java program has at least one class.
+-The entry point is the main method:
+public static void main(String[] args)
+
+2. Variables and Data Types:
+-Variables store data.
+Primitive types:
+    -int (integer numbers)
+    -double (decimal numbers)
+    -char (single characters)
+    -boolean (true/false)
+    -byte, short, long, float (other numeric types)
+
+3. Operators:
+    -Arithmetic: +, -, *, /, %
+    -Relational: ==, !=, >, <, >=, <=
+    -Logical: && (and), || (or), ! (not)
+ */
+
+public class javaBasics {
+    
+}
