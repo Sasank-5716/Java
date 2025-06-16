1da252481a6f6e62119f0e595beaaeeb735ccb8a
Sasank5716 <sasanklama5716@gmail.com>
2025-06-16 16:56:10 +0545

    Add demo for java basics(variables, datatypes, expressions and operators)

--- Java_basics/javaBasics.java
+++ Java_basics/javaBasics.java
@@ -20,5 +20,13 @@ Primitive types:
  */
 
 public class javaBasics {
-    
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+
+        boolean isGreater = a > b;
+        System.out.println("Is a greater than b? " + isGreater);
+    }
 }
