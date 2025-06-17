626364bc6ee9c5537ccdacb8429f6a91991b9cd2
Sasank5716 <sasanklama5716@gmail.com>
2025-06-17 16:53:10 +0545

    Add theory for methods and arrays

---
+++ Java_basics/Methods_and_array.java
@@ -0,0 +1,35 @@
+/* 
+Methods
+Definition: A method is a block of code that performs a specific task and can be called (invoked) from elsewhere in your program.
+
+Syntax:
+returnType methodName(parameters) {
+    // code
+    return value; // if returnType is not void
+}
+
+
+Method Overloading
+Defining multiple methods with the same name but different parameters.
+
+Example:
+int add(int a, int b) { return a + b; }
+double add(double a, double b) { return a + b; }
+Recursion
+A method that calls itself.
+Useful for problems that can be broken down into similar subproblems.
+
+
+Arrays
+Definition: Arrays are containers that hold a fixed number of values of a single type.
+
+Declaration:
+int[] numbers = new int[5]; // 1D array
+int[][] matrix = new int[3][3]; // 2D array
+*/
+
+public class Methods_and_array {
+    public static void main(String[] args) {
+       
+    }
+}
