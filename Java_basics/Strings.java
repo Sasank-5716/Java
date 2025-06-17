0c0ae714e4b6986fd7a6c88dfce4266473aaa02f
Sasank5716 <sasanklama5716@gmail.com>
2025-06-17 16:57:56 +0545

    Add String example

--- Java_basics/Strings.java
+++ Java_basics/Strings.java
@@ -21,3 +21,19 @@ Scanner sc = new Scanner(System.in);
 String input = sc.nextLine();
 int num = sc.nextInt();
  */
+
+ import java.util.Scanner;
+
+public class Strings {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+
+        String s1 = "Java";
+        String s2 = "Programming";
+        String s3 = s1 + " " + s2;
+        System.out.println(s3.toUpperCase());
+    }
+}
