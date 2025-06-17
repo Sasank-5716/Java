f8b2faefd075015a7865aeca1e4847361dcc2911
Sasank5716 <sasanklama5716@gmail.com>
2025-06-17 16:57:07 +0545

    Add theory for string

---
+++ Java_basics/Strings.java
@@ -0,0 +1,23 @@
+/*
+ * Strings and Basic I/O (7–9 hours)
+Theory
+String Class
+Strings in Java are objects (not primitive types).
+Created using double quotes: String name = "Java";
+StringBuilder/StringBuffer: Used for mutable strings (when you need to modify the string frequently).
+
+Common String Operations
+.length(), .charAt(), .substring(), .equals(), .compareTo(), .concat()
+
+Example:
+String s = "Hello";
+System.out.println(s.length()); // 5
+System.out.println(s.substring(1, 4)); // "ell"
+
+Basic Input/Output
+Use Scanner for input from the keyboard.
+import java.util.Scanner;
+Scanner sc = new Scanner(System.in);
+String input = sc.nextLine();
+int num = sc.nextInt();
+ */
