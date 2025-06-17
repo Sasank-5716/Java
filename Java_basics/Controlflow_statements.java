f75f408d17ae0ae92b19775909e0d1c456da8d70
Sasank5716 <sasanklama5716@gmail.com>
2025-06-17 16:42:51 +0545

    Add controlflow-statements theory

---
+++ Java_basics/Controlflow_statements.java
@@ -0,0 +1,25 @@
+/*Control Flow Statements 
+Conditional Statements:
+
+if, else if, else control flow based on conditions.
+
+switch allows multi-way branching based on a variable.
+
+Loops:
+
+for loop: repeats a block a fixed number of times.
+
+while loop: repeats while a condition is true.
+
+do-while loop: executes at least once, then repeats while condition is true.
+
+Enhanced for loop: used to iterate over arrays or collections.
+
+Control Keywords:
+
+break: exits a loop or switch.
+
+continue: skips current iteration.
+
+return: exits a method.
+*/
