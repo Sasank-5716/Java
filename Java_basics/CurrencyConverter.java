e9a1651b9a99ad84e5cbe48501202b2f6878419a
Sasank5716 <sasanklama5716@gmail.com>
2025-06-19 16:45:03 +0545

    Add user amount and source currency input

---
+++ Java_basics/CurrencyConverter.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CurrencyConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input the amount to convert
+        System.out.print("Enter the amount: ");
+        double amount = scanner.nextDouble();
+
+        // Input the source currency
+        System.out.print("Enter the currency to convert from (USD, EUR, INR): ");
+        String fromCurrency = scanner.next().toUpperCase();
+
+    
+    }
+}
+
