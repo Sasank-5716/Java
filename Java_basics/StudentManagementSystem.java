f0074dd567b58393411f9885e5218adf4cb343cc
Sasank5716 <sasanklama5716@gmail.com>
2025-06-21 00:19:21 +0545

    Add user choice

--- Java_basics/StudentManagementSystem.java
+++ Java_basics/StudentManagementSystem.java
@@ -99,6 +99,18 @@ public class StudentManagementSystem {
          Scanner scanner = new Scanner(System.in); 
         StudentManager manager = new StudentManager(); 
 
-        
+        while (true) { 
+            System.out.println("\n--- STUDENT MANAGEMENT SYSTEM ---");
+            System.out.println("1. Add Student");
+            System.out.println("2. View All Students");
+            System.out.println("3. Update Student");
+            System.out.println("4. Delete Student");
+            System.out.println("5. Search Student by Name");
+            System.out.println("6. Exit");
+            System.out.print("Enter your choice: ");
+            
+            int choice = scanner.nextInt(); 
+            scanner.nextLine();  
+        }
     }
 }
