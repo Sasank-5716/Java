f0dc0b8603a897b33d1671a4758a34691ac31ee4
Sasank5716 <sasanklama5716@gmail.com>
2025-06-20 15:25:03 +0545

    Add student class to store student data

---
+++ Java_basics/Student_management_system.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// Student class to store student data
+class Student {
+    private int id;
+    private String name;
+    private String grade;
+
+    public Student(int id, String name, String grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getGrade() { return grade; }
+
+    public void setName(String name) { this.name = name; }
+    public void setGrade(String grade) { this.grade = grade; }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Name: " + name + ", Grade: " + grade;
+    }
+}
+
+public class Student_management_system {
+    
+}
